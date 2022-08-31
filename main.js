b3a9b7050db5a958e2007b2307b517392284537a
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-08-31 02:43:29 -0300

    terminando desafio nft

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ function randomChallenger(){
     const paginas = 
     ['interactive-rating-component-main',
      'qr-code-component-main',
-     'product-preview-card-component-main'];
+     'product-preview-card-component-main',
+     'nft-preview-card-component-main'];
     const pagina = Math.floor(Math.random() * paginas.length);
 
     window.open( `./${paginas[pagina]}`, '_blank')
