3cfbb16e2f943db3bb6302cbc48d3554aac05ae3
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-09-02 17:45:09 -0300

    iniciando fetch e construindo cards a partir da api

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const paragrafo = document.getElementById('paragrafo');
+const url = "https://api.github.com/repos/felipejesus-front/Desafios-frontend/contents"
+const cardsContainer = document.querySelector('#contentList');
 
 // função reutilizável com qualquer outro elemento de texto
 function typeWriter(elemento, intervalo) {
     const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ''
+    elemento.innerHTML = '';
     
     textoArray.forEach((letra, i) => {
         setTimeout(() => elemento.innerHTML += letra, intervalo * i)
@@ -21,3 +23,35 @@ function randomChallenger(){
 
     window.open( `./${paginas[pagina]}`, '_blank')
 }
+
+function criarCardHTML(pasta) {
+    const stringPura = pasta.name;
+    const stringTratada = (stringPura.charAt(0).toUpperCase() + stringPura.slice(1)).replace(/-/g,' ');
+    console.log(stringTratada)
+
+    const card = `        <div class="card-body">
+    <a href="./${pasta.name}"><img src="https://raw.githubusercontent.com/felipejesus-front/Desafios-frontend/main/${pasta.name}/design/desktop-preview.jpg" alt=""></a>
+    <h3><a href="./${pasta.name}">${stringTratada}</a></h3>
+    <a href="./${pasta.name}">Ir para a pagina</a>
+    </div>
+    `;
+    
+    return card;
+  }
+
+function preencherCard(diretorio) {
+    const divCard = criarCardHTML(diretorio);
+    cardsContainer.innerHTML += divCard;
+  }
+
+async function criarCards(){
+    const repositoryDataResponse = await fetch(url);
+    const repositoryJson = await repositoryDataResponse.json();
+    repositoryJson.forEach((item)=>{
+
+        if(item.type === "dir"){
+            preencherCard(item)
+        }
+    })
+}
+criarCards();
